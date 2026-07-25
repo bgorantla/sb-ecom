f9ccbd98451c59a4299b772c95bd51cedd84a52f
bgorantla <bgorantla500@gmail.com>
2026-07-25 04:08:21 -0700

    Fixed bugs

--- ecom-frontend/src/App.jsx
+++ ecom-frontend/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Products from './components/Products'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Products />
