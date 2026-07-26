4528762b2d18482aff4597e721e6052db8dcff8f
bgorantla <bgorantla500@gmail.com>
2026-07-26 01:43:34 -0700

    Redux configuration for payment method selection

--- ecom-frontend/src/store/reducers/store.js
+++ ecom-frontend/src/store/reducers/store.js
@@ -3,10 +3,11 @@ import { productReducer } from "./ProductReducer";
 import { errorReducer } from "./errorReducer";
 import { cartReducer } from "./cartReducer";
 import { authReducer } from "./authReducer";
+import { paymentMethodReducer } from "./paymentMethodReducer";
 
 const user = localStorage.getItem("auth")
     ? JSON.parse(localStorage.getItem("auth"))
-    : [];
+    : null;
 
 const cartItems = localStorage.getItem("cartItems")
     ? JSON.parse(localStorage.getItem("cartItems"))
@@ -23,6 +24,7 @@ export const store = configureStore({
         errors: errorReducer,
         carts: cartReducer,
         auth: authReducer,
+        payment: paymentMethodReducer,
     },
     preloadedState: initialState,
 });
